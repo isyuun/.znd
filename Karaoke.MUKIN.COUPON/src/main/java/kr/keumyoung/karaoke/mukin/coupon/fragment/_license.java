e33c565e7ead2e42aebb42482c5f85b77450ec3b
isyuun <isyuun@keumyoung.kr>
2018-11-14 16:56:03 +0900

    14차 - 1.4(14) -BUG: 블루투스 검색시 다운오류 개선 -BUG: 재로그인 갱신문제 개선 -CHG: 패키지파일 확인기능 -CHG: 세션업데이트시 화면갱신개선

--- Karaoke.MUKIN.COUPON/src/main/java/kr/keumyoung/karaoke/mukin/coupon/fragment/_license.java
+++ Karaoke.MUKIN.COUPON/src/main/java/kr/keumyoung/karaoke/mukin/coupon/fragment/_license.java
@@ -3,10 +3,10 @@ package kr.keumyoung.karaoke.mukin.coupon.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 
-public class _version extends html {
+public class _license extends html {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        myWebView.loadUrl("https://www.keumyoung.kr:444/mukinapp/_version.html");
+        myWebView.loadUrl("https://www.keumyoung.kr:444/mukinapp/_LICENSE.html");
     }
 }
---
+++ Karaoke.MUKIN.COUPON/src/main/java/kr/keumyoung/karaoke/mukin/coupon/fragment/_release.java
@@ -0,0 +1,12 @@
+package kr.keumyoung.karaoke.mukin.coupon.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+public class _release extends html {
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        myWebView.loadUrl("https://www.keumyoung.kr:444/mukinapp/_RELEASE.html");
+    }
+}
