968fe9685af3b501e473aecda59c0c215e352805
isyuun <isyuun@gmail.com>
2024-07-19 13:42:50 +0900

    ...

---
+++ Karaoke.PLAY.APP/src/kr/kymedia/karaoke/play/apps/test/MainActivity2.java
@@ -0,0 +1,14 @@
+package kr.kymedia.karaoke.play.apps.test;
+
+import android.os.Bundle;
+
+import kr.kymedia.karaoke.app.AppCompatActivity;
+import kr.kymedia.karaoke.play.app.R;
+
+public class MainActivity2 extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
